cf6821bfefb3f5255869b78ece585f42b4d422a7
roger10e <rogertenny@gmail.com>
2022-04-07 19:06:06 -0500

    added comments to js file with project requirements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//Requirements: 
+//Use a CSS framework other than Bootstrap
+//Be deployed to GH pages
+//Be interactive (accept & respond to user input)
+//Use at least two server-side APIs
+//Does not use alert, confirms, or prompts
+//Use client-side storage to store persistent data
+//Be responsive
+//Have a polished UI
+//Have a clean repo that meets quality coding standards (file structure, naming conventions, follows best practices for class/id naming conventions, indentation, quality comments, etc.)
+//Have a quality README (with unique name, description, tech used, screenshot, and link to deployed application)
